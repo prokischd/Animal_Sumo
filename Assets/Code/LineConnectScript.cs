7b0edd73b63523cf738248391d5e686c6b055a2b
prokischd <prokischd@gmail.com>
2020-03-06 18:16:02 +0100

    player character template

---
+++ Assets/Code/LineConnectScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineConnectScript : MonoBehaviour
+{
+
+    private LineRenderer myLine;
+    private GameObject[] childObjs;
+
+    void Start()
+    {
+        SetInitialReferences();
+    }
+
+    void Update()
+    {
+        SetLine();
+    }
+
+    void SetInitialReferences() {
+        myLine = GetComponent<LineRenderer>();
+        childObjs = new GameObject[transform.childCount];
+        myLine.positionCount = childObjs.Length;
+        for (int i = 0; i < childObjs.Length; i++)
+        {
+            childObjs[i] = transform.GetChild(i).gameObject;
+            myLine.SetPosition(i, childObjs[i].transform.position);
+        }
+
+
+    }
+
+
+    void SetLine() {
+        for (int i = 0; i < childObjs.Length; i++)
+        {
+            myLine.SetPosition(i, childObjs[i].transform.position);
+        }
+    }
+}
