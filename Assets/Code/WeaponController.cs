ae483d2c6856484aa746f38efd42fe91f15745f3
lacee <laci96zenta@gmail.com>
2020-03-06 20:04:13 +0100

    sdgdfghfj

---
+++ Assets/Code/WeaponController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+	public CircleCollider2D bodyCollider;
+	public GameObject weaponSpot;
+	public List<Weapon> weapons;
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.gameObject.GetComponent<Weapon>() is Weapon weapon)
+		{
+			weapon.transform.parent = weaponSpot.transform;
+			weapon.transform.localPosition = Vector3.zero;
+			weapons.Add(weapon);
+		}
+	}
+
+
+	private void Update()
+	{
+		foreach(var wep in weapons)
+		{
+			if(wep.CanExecute())
+			{
+
+			}
+		}
+	}
+}
