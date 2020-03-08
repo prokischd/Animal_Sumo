ad6eea8700811aa6ab23c14ddace4d4c926c4df8
prokischd <prokischd@gmail.com>
2020-03-08 07:03:17 +0100

    level set up more

---
+++ Assets/Code/WeaponS/WaterFlow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFlow : MonoBehaviour
+{
+
+    public float scrollSpeed = 0.5f;
+    private LineRenderer myline;
+    private float offset;
+
+    void Start()
+    {
+        myline = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        offset += Time.deltaTime * scrollSpeed;
+        myline.material.SetTextureOffset("_MainTex", new Vector2(offset,0));
+        if (Mathf.Abs(offset) >= 1)
+        {
+            offset = 0;
+        }
+    }
+}
